73b41f8e2b1aeb3bca9bfd3574f0ee9b370da68f
Michax <michax@gmail.com>
2016-11-27 23:36:23 +0100

    Fix react warnings

--- src/Form.js
+++ src/Form.js
@@ -12,7 +12,7 @@ class Form extends React.Component {
                     <span className="input-group-addon" id="basic-addon2">@example.com</span>
                 </div>
                 <br />
-                <label for="basic-url">Your vanity URL</label>
+                <label htmlFor="basic-url">Your vanity URL</label>
                 <div className="input-group">
                     <span className="input-group-addon" id="basic-addon3">https://example.com/users/</span>
                     <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
